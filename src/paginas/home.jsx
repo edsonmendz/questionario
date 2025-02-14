1db7bd9aa78bbb667d8069920925b964ff42a2b7
edsonmendz <75332178+edsonmendz@users.noreply.github.com>
2025-02-14 15:31:37 -0300

    confirmando alterações

--- src/paginas/home.jsx
+++ src/paginas/home.jsx
@@ -5,7 +5,7 @@ function Home() {
         <div>
             <h1>Questionário Transporte de Produtos Perigosos</h1>
             <br />
-            <p>Preare-se para as perguntas do Transporte de Produtos perigosos com este simulado</p>
+            <p>Preare-se para 100 perguntas do Transporte de Produtos perigosos com este simulado</p>
             <br />
             <Link to={'/perguntas'}><Botao text='Começar' classe={'esperando largura50'} /></Link>
             <p>por:Edson Mendes</p>
